b00cd6e2d1cf81b5c26f9b0b9f8ea28422203723
William Grohe <william.grohe@gmail.com>
2021-07-29 14:50:28 -0300

    Finish interactions on link of menu

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+/* abrir e esconder o menu toggle/hamburguer */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for( let element of toggle) {
     element.addEventListener('click', ()=>{
         nav.classList.toggle('show')
+    })
+}
+
+/* fechar o menu ao clicar no link/seção */
+const menu = document.querySelectorAll('.menu ul')
+
+for (let link of menu){
+    link.addEventListener('click', ()=>{
+        nav.classList.remove('show')
     })
 }
